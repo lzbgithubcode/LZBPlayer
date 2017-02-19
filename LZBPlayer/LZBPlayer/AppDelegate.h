60d3935f273cc8c7a1216c0a65fc3962f2d97444
lzbgithubcode <1835064412@qq.com>
2017-02-19 17:52:36 +0800

    初始化

---
+++ LZBPlayer/LZBPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZBPlayer
+//
+//  Created by Apple on 2017/2/19.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZBPlayer/LZBPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZBPlayer
+//
+//  Created by Apple on 2017/2/19.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
